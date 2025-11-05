ade6ca62d0803d05b353a2a0d9b9b60f43c49a61
Martí Juanola <42376790+martijuanola@users.noreply.github.com>
2025-11-05 14:47:46 +0100

    [E2] - code reformat of ex2 & ex3 + store output ex2

--- exercices/e2/ex3.py
+++ exercices/e2/ex3.py
@@ -4,25 +4,25 @@ import pandas as pd
 
 
 def count_words(filepath, target_words):
-    #load file
+    # load file
     with open(filepath, 'r', encoding='latin1') as f:
-        #remove header
+        # remove header
         f.readline()
 
         full_text = f.read()
-    #convert all word to lower case
+    # convert all word to lower case
     all_text_lower = full_text.lower()
-    #tokenize words
+    # tokenize words
     words = re.findall(r'\b\w+\b', all_text_lower)
-    #count all words
+    # count all words
     all_words_nr = len(words)
-    #turn list into pandas column
+    # turn list into pandas column
     word_series = pd.Series(words)
-    #filter out the words we want to count
+    # filter out the words we want to count
     target_word_series = word_series[word_series.isin(target_words)]
-    #count the words
+    # count the words
     word_counts = target_word_series.value_counts()
-    #make sure that all target words are returned, especially the ones that have 0 occurences
+    # make sure that all target words are returned, especially the ones that have 0 occurences
     final_counts = word_counts.reindex(target_words, fill_value=0)
 
     return final_counts, all_words_nr
@@ -32,19 +32,19 @@ if __name__ == "__main__":
     FILE_PATH = 'data/lyrics50.csv'
     TARGET_WORDS = ['always', 'but', 'christmas', 'city', 'feel', 'love', 'think', 'you', 'world', 'yeah']
 
-    #get pandas series where every words has its occurence nr next to it
+    # get pandas series where every words has its occurence nr next to it
     counts, all_words_nr = count_words(FILE_PATH, TARGET_WORDS)
 
-    #calculate tf for every entry in counts series
+    # calculate tf for every entry in counts series
     tf = counts / all_words_nr
 
-    #calculate idf, but we have only one document, where every word is contained so log(1/1) = 0
+    # calculate idf, but we have only one document, where every word is contained so log(1/1) = 0
     idf = 0
 
-    #calculate tf -df
+    # calculate tf -df
     tf_idf = tf * idf
 
-    #print solutions
+    # print solutions
     print("target words:")
     print(TARGET_WORDS)
     print("")
