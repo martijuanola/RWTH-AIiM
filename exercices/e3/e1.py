efc3e0642ce551574974e17cd1f143c230d7565e
Andreas Rogalski <andreas.rogalski@rwth-aachen.de>
2025-11-06 18:34:52 +0100

    exercise 3.1

---
+++ exercices/e3/e1.py
@@ -0,0 +1,28 @@
+import librosa
+import matplotlib.pyplot as plt
+
+
+
+
+if __name__ == "__main__":
+
+    #load file
+    filepath = "data/classical.mp3"
+    y , sr = librosa.load(filepath)
+
+    #get onsets and their times
+    onsets = librosa.onset.onset_detect(y=y,sr=sr)
+    onset_times = librosa.frames_to_time(onsets,sr=sr)
+    print(onset_times)
+
+    #plot everything
+    plt.figure()
+    ax = plt.subplot(1,1,1)
+
+    librosa.display.waveshow(y, sr=sr, ax=ax)
+    
+    ax.vlines(onset_times, -1, 1, linestyle='-',alpha = 0.25,color="orange")
+    ax.set_xlabel("time in seconds")
+    ax.set_ylabel("amplitude")
+
+    plt.show()
