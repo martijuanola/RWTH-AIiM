d4e6fc953fd2550fd44e0d223a05135493e3051a
Martí Juanola <42376790+martijuanola@users.noreply.github.com>
2025-11-07 12:50:11 +0100

    Add exercise pdfs for EX2 & EX3

--- exercices/e3/e1.py
+++ exercices/e3/e1.py
@@ -1,29 +1,24 @@
 import librosa
 import matplotlib.pyplot as plt
 
-
-
-
 if __name__ == "__main__":
-
     #load file
     filepath = "data/classical.mp3"
-    y , sr = librosa.load(filepath)
+    y, sr = librosa.load(filepath)
 
     #get onsets and their times
-    onsets = librosa.onset.onset_detect(y=y,sr=sr)
-    onset_times = librosa.frames_to_time(onsets,sr=sr)
+    onsets = librosa.onset.onset_detect(y=y, sr=sr)
+    onset_times = librosa.frames_to_time(onsets, sr=sr)
     print(onset_times)
 
     #plot everything
     plt.figure()
-    ax = plt.subplot(1,1,1)
+    ax = plt.subplot(1, 1, 1)
     ax.set_facecolor("#333333")
 
+    librosa.display.waveshow(y, sr=sr, ax=ax, alpha=0.8)
 
-    librosa.display.waveshow(y, sr=sr, ax=ax, alpha = 0.8)
-    
-    ax.vlines(onset_times, -1, 1, linestyle="-",alpha = 0.05,color="yellow")
+    ax.vlines(onset_times, -1, 1, linestyle="-", alpha=0.05, color="yellow")
     ax.set_xlabel("time in seconds")
     ax.set_ylabel("amplitude")
 
